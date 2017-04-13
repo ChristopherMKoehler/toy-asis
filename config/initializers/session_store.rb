d94c105c6e5957030c75ff09f199bc6bbae5517f
Lenny Marks <lenny@aps.org>
2017-04-13 16:16:11 -0400

    Rails 4.2.8

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ToyAsis::Application.config.session_store :cookie_store, key: '_toy-asis_session'
+Rails.application.config.session_store :cookie_store, key: '_toy_asis_session'
