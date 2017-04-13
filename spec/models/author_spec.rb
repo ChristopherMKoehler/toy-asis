0d3d63c7fbb326ff4788cf1e849011b0288f1f58
Lenny Marks <lenny@aps.org>
2017-04-13 18:25:41 -0400

    Add broken spec

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,15 +1,27 @@
 require 'spec_helper'
 
 describe Author do
-  describe '#last_name is derived from :publish_name' do
+  describe '#last_name is derived from first component of comma separated :publish_name' do
+    subject(:author) { Author.new }
+
     example 'Smith from Smith,Joe' do
-      subject.publish_name = 'Smith,Joe'
-      expect(subject.last_name).to eql('Smith')
+      author.publish_name = 'Smith,Joe'
+      expect(author.last_name).to eql('Smith')
     end
 
     example 'Smith from Smith,Joe,M' do
-      subject.publish_name = 'Smith,Joe,M'
-      expect(subject.last_name).to eql('Smith')
+      author.publish_name = 'Smith,Joe,M'
+      expect(author.last_name).to eql('Smith')
+    end
+
+    it 'strips leading/trailing white-space' do
+      author.publish_name = " \t\nSmith , Joe"
+      expect(author.last_name).to eq('Smith')
+    end
+
+    it 'keeps embedded white-space' do
+      author.publish_name = 'Smith Jr.,Joe'
+      expect(author.last_name).to eq('Smith Jr.')
     end
   end
 end
