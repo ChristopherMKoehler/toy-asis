e64cfcc1222462df10820bb2f9dc76f85fa12c63
Lenny Marks <lenny@aps.org>
2017-04-13 16:45:48 -0400

    rspec 2.99/mongoid-5

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,22 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "rails"
+ 
+%w(
+  #active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  #active_job/railtie
+  #action_cable/engine
+  #rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require "#{railtie}"
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,6 +37,6 @@ module ToyAsis
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
