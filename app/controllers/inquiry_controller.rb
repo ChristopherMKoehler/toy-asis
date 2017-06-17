7fc35dbfdbe3e8236d5232843e9dd09ec3a10bbc
Chris Koehler <christophermnkoehler@gmail.com>
2017-06-17 17:22:03 -0400

    Restructure routes of inquiry and manuscript controllers to ahere to standards

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -1,8 +1,38 @@
 class InquiryController < ApplicationController
-  def index
+  def new
+    flash[:errors] = ""
     @errors = params[:errors]
-    @lname = params[:lname]
+    @lname = params[:last_name]
     @code = params[:code]
-    render :index
+
+    if params[:submitted]
+      @manuscript = Manuscript.find_manuscript(@code, @lname)
+      if @manuscript
+        redirect_to manuscript_url(@manuscript.id)
+      else
+        show_with_errors
+      end
+    else
+      render :new
+    end
+  end
+
+  def show_with_errors
+    flash[:errors] = "Manuscript not found."
+    @lname = params[:last_name]
+    @code = params[:code]
+
+    if @lname && @lname.length <= 1
+      flash[:errors] = "Last name search parameter must be at least 2 characters long."
+    end
+    if @code && @code.length === 0
+      if flash[:errors] === "Manuscript not found."
+        flash[:errors] = " Code cannot be blank."
+      else
+        flash[:errors] << " Code cannot be blank."
+      end
+    end
+
+    render :new
   end
 end
--- app/controllers/manuscript_controller.rb
+++ app/controllers/manuscript_controller.rb
@@ -1,30 +1,11 @@
 class ManuscriptController < ApplicationController
-  def index
-    @manuscript = Manuscript.find_manuscript(params[:code], params[:last_name])
+  def show
+    @manuscript = Manuscript.find(params[:id])
 
     if @manuscript
       render :show
     else
-      error_redirect
+      redirect_to new_inquiry_url
     end
-  end
-
-  def error_redirect
-    flash[:errors] = "Manuscript not found."
-    lname = params[:last_name]
-    code = params[:code]
-
-    if lname && lname.length <= 1
-      flash[:errors] = "Last name search parameter must be at least 2 characters long."
-    end
-    if code && code.length === 0
-      if flash[:errors] === "Manuscript not found."
-        flash[:errors] = " Code cannot be blank."
-      else
-        flash[:errors] << " Code cannot be blank."
-      end
-    end
-
-    redirect_to inquiry_index_url(lname: lname, code: code)
   end
 end
